d993c9ffd82d29cbb47397e3224e975d471e357c
blackdevelopa <blackdevelopa@gmail.com>
2019-08-03 11:01:23 +0100

    added navigation using react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { Route, Switch} from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component'
+
+const HatsPage = () => (
+  <div>
+    <h1>HatsPage</h1>
+  </div>
+) 
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/shop/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
