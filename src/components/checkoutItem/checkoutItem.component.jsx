606c477345afc26eb2d756f135d2d1d109e01adf
blackdevelopa <blackdevelopa@gmail.com>
2019-08-12 14:49:40 +0100

    used redux persist to persist and rehydrate redux store

--- src/components/checkoutItem/checkoutItem.component.jsx
+++ src/components/checkoutItem/checkoutItem.component.jsx
@@ -12,9 +12,9 @@ const CheckoutItem = ({ removeFromCart, cartItem, removeItem, addItem }) => {
       </div>
       <span className="name">{name}</span>
       <span className="quantity">
-        <div className="value" onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
           {quantity}
-        <div className="value" onClick={() => addItem(cartItem)}>&#10095;</div>
+        <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
       </span>
       <span className="price">{price}</span>
       <div className="remove-button" onClick={() => removeFromCart(cartItem)}>&#10005;</div>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
+import { persistStore } from "redux-persist";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 
@@ -8,4 +9,6 @@ const middlewares = [logger];
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)) );
 
-export default store;
+const persistor = persistStore(store);
+
+export {store, persistor};
