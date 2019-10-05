1837d7476c23901c788dd63fb6a598eab035ca08
blackdevelopa <blackdevelopa@gmail.com>
2019-10-05 22:04:49 +0100

    revert changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from "redux-persist";
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)) );
+const store = createStore(rootReducer, (applyMiddleware(...middlewares)) );
 
 const persistor = persistStore(store);
 
